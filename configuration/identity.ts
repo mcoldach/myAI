37b264fa9f383cba8f13289ca299dcbc46e61024
mcoldach <mcoldach@gmail.com>
2025-02-19 16:42:43 -0500

    Update identity.ts
    Change to running.

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Michael Oldach`;
 export const OWNER_DESCRIPTION: string = `Learner, Entrepreneur and AI Enthusiast`;
 
-export const AI_NAME: string = `IronTriangleAI`;
-export const AI_TONE: string = `Concise, Engaging, Diplomatic, Structured, and Persuasive`;
-export const AI_ROLE: string = `Project Management Assistant`;
+export const AI_NAME: string = `VDOT AI`;
+export const AI_TONE: string = `Eager, Encouraging, Informative`;
+export const AI_ROLE: string = `Running Coach Expert`;
