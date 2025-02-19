f533c8dfac4f04a5df6961cabf4f55660348d9b3
mcoldach <mcoldach@gmail.com>
2025-02-19 16:48:24 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `IronTriangleAI`;
+export const CHAT_HEADER: string = `VDOT AI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `IronTriangleAI can make mistakes. Check important info.`;
+export const FOOTER_MESSAGE: string = `VDOT AI can make mistakes. Check important info.`;
 export const CLEAR_BUTTON_TEXT: string = `Start Over`;
-export const PAGE_TITLE: string = `IronTriangleAI`;
+export const PAGE_TITLE: string = `VDOT AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
